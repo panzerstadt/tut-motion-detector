7fccbcb357c96ca2c8b9b2963fcf06ad91573d77
panzerstadt <tliqun@gmail.com>
2019-05-19 16:55:25 +0900

    Updates

---
+++ precache-manifest.a0cb2b471851160715418fc5ec8c264b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b659dfab9a67e233fbcb6a9fb47a7af4",
+    "url": "/tut-motion-detector/index.html"
+  },
+  {
+    "revision": "986302e644df700578ba",
+    "url": "/tut-motion-detector/static/css/main.b2a3e1c8.chunk.css"
+  },
+  {
+    "revision": "f3b6d5276078e818426f",
+    "url": "/tut-motion-detector/static/js/2.408cda65.chunk.js"
+  },
+  {
+    "revision": "986302e644df700578ba",
+    "url": "/tut-motion-detector/static/js/main.3bd2ddea.chunk.js"
+  },
+  {
+    "revision": "9ad3db5c3a7436a2e99c",
+    "url": "/tut-motion-detector/static/js/runtime~main.2ef15dd0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tut-motion-detector/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.594d032a184bebb178fd5244e0d9651d.js"
+  "/tut-motion-detector/precache-manifest.a0cb2b471851160715418fc5ec8c264b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tut-motion-detector/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
